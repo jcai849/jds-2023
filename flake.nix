d273d34b2e90796fef0695d3943ef5300fa9f140
Jason Cairns <jason@cair.nz>
2025-04-18 11:21:33 +1200

    dev: Reproducible compilation
    - add flake and direnv
    - add caches to gitignore

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bmake
+          texlive.combined.scheme-full
+          texlab
+          graphviz
+          dot2tex
+        ];
+      };
+    };
+}
